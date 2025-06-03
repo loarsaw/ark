61fe3c2133d0bd556645798504a9adc597b0f794
loarsaw <amanahmed1385@gmail.com>
2025-06-03 08:51:11 +0530

    origin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,9 @@ import cors from 'cors'
 dotenv.config();
 const app = express();
 app.use(json())
-app.use(cors())
+app.use(cors({
+  origin: "*"
+}))
 const PORT = process.env.PORT;
 
 app.get("/", (request: Request, response: Response) => {
