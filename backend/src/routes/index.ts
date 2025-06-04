ba2b395164249c43093e12216f6b1c804eea5d24
loarsaw <amanahmed1385@gmail.com>
2025-06-04 10:49:23 +0530

    minor update

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,12 +3,14 @@ import { signup } from "../controllers/auth/sign-up/signup";
 import { signin } from "../controllers/auth/sign-in/signin";
 import { createTask, deleteTask, getTasks, updateTask } from "../controllers/tasks/tasks";
 import { verify } from "../controllers/auth/verify/verify";
+import { getPublicProfile } from "../controllers/auth/profile/profile";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/verify", verify);
+router.post("/user", getPublicProfile)
 router.get("/tasks", getTasks);
 router.post("/task", createTask);
 router.put("/update-task", updateTask);
